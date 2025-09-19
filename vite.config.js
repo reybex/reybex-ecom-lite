b69df8200cb7d61062460a6982df4388763e2440
Mert Adatepe <madatepe@reybex.com>
2025-09-19 17:37:07 +0300

    Deploy with fixed file names - no more hash changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   plugins: [vue()],
   base: '/reybex-ecom-lite/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
